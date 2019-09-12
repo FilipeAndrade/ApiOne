a47a92939f5f9751dae25036bdceecb87c921c81
Filipe Andrade <filipe.dsandrade@gmail.com>
2019-09-11 22:19:21 -0300

    Add project files.

---
+++ ApiOne/Controllers/TaxaJurosController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ApiOne.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TaxaJurosController : ControllerBase
+    {
+        
+    }
+}
